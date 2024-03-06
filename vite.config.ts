67315d0161677a28f9b5960d95dd534cfc3677ff
RAUL AILLON <raulaillon@gmail.com>
2024-03-06 00:03:54 -0400

    added vitest dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,21 @@ import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 export default defineConfig({
   plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: ["./test/setup-test-env.ts"],
+    include: ["./app/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    watchExclude: [
+        ".*\\/node_modules\\/.*",
+        ".*\\/build\\/.*",
+        ".*\\/postgres-data\\/.*",
+    ],
+    coverage: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80
+    }    
+  },
 });
