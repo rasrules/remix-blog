0e94f1de8fbc820dd4f49bac4cbc4486effb065b
RAUL AILLON <raulaillon@gmail.com>
2024-03-13 22:07:53 -0400

    updated testing dependencies

--- app/Healthcheck.test.tsx
+++ app/Healthcheck.test.tsx
@@ -1,22 +1,23 @@
-import { enableFetchMocks } from 'jest-fetch-mock';
-import '@testing-library/jest-dom'; // So we can use toBeInTheDocument assertion
+import "@testing-library/jest-dom"; // So we can use toBeInTheDocument assertion
+import { loader } from "./routes/healthcheck";
 
-import { loader } from './routes/healthcheck';
-
-
-enableFetchMocks();
-it('should show OK message', async () => {
-    const testOkMessage = 'OK';
-    const response = await loader(undefined);
-    expect(response.status).toBe(200);
-    expect(response.statusText).toBe(testOkMessage);
-
+it("should show OK message", async () => {
+  const testOkMessage = "OK";
+  const response = await loader(undefined);
+  expect(response.status).toBe(200);
+  expect(response.statusText).toBe(testOkMessage);
 });
 
-it('should show test ERROR message', async () => {
-    const testErrorMessage = 'test ERROR';
-    const fake = () => Promise.resolve(new Response(testErrorMessage, { status: 500, statusText: testErrorMessage }));
-    const response = await loader({create: fake});
-    expect(response.status).toBe(500);
-    expect(response.statusText).toBe(testErrorMessage);
+it("should show test ERROR message", async () => {
+  const testErrorMessage = "test ERROR";
+  const fake = () =>
+    Promise.resolve(
+      new Response(testErrorMessage, {
+        status: 500,
+        statusText: testErrorMessage,
+      }),
+    );
+  const response = await loader({ create: fake });
+  expect(response.status).toBe(500);
+  expect(response.statusText).toBe(testErrorMessage);
 });
--- app/useCases/checkServiceState/NotionServiceState.test.tsx
+++ app/useCases/checkServiceState/NotionServiceState.test.tsx
@@ -1,20 +1,16 @@
-import { enableFetchMocks } from 'jest-fetch-mock';
-import { verifyStatusResponse } from './NotionServiceState';
-import { SPAN_CLASSES, STATUS_TEXT} from './NotionServiceConstants';
+import { verifyStatusResponse } from "./NotionServiceState";
+import { SPAN_CLASSES, STATUS_TEXT } from "./NotionServiceConstants";
 
-
-enableFetchMocks();
-it('should finish OK', () => {
-    expect(() => {
-      const htmlText: string = `<html><body><p class='${SPAN_CLASSES}'>${STATUS_TEXT}</p></body>`;
-      verifyStatusResponse(htmlText);
-    }).not.toThrow(Error);
-
+it("should finish OK", () => {
+  expect(() => {
+    const htmlText: string = `<html><body><p class='${SPAN_CLASSES}'>${STATUS_TEXT}</p></body>`;
+    verifyStatusResponse(htmlText);
+  }).not.toThrow(Error);
 });
 
-it('should throw error', () => {
-    expect(() => {
-      const htmlText: string = "<html><body><p class='test'>Test Text</p></body>";
-      verifyStatusResponse(htmlText)
-    }).toThrow(Error)    
-})
+it("should throw error", () => {
+  expect(() => {
+    const htmlText: string = "<html><body><p class='test'>Test Text</p></body>";
+    verifyStatusResponse(htmlText);
+  }).toThrow(Error);
+});
